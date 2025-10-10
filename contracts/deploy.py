a36233f3272aaa6a3cb4203778f9b2615414274e
D,Shashank <shashankdumpala6@gmail.com>
2025-10-10 23:32:36 +0530

    Add Pyteal contract with release time logic

--- contracts/deploy.py
+++ contracts/deploy.py
@@ -1,3 +1,4 @@
+# contracts/deploy.py
 import sys
 from pyteal import compileTeal, Mode
 from algosdk import account, algod, transaction
@@ -9,31 +10,25 @@ def compile_contract():
         f.write(teal)
     return "inheritance.teal"
 
-def deploy_app(algod_client, creator_private_key, approval_path, clear_path):
+def deploy_app(algod_client, creator_private_key, approval_path):
     sender = account.address_from_private_key(creator_private_key)
     with open(approval_path, "rb") as f:
         approval = f.read()
-    with open(clear_path, "rb") as f:
-        clear = f.read()
+    clear = approval
     params = algod_client.suggested_params()
     txn = transaction.ApplicationCreateTxn(
         sender, params, on_complete=transaction.OnComplete.NoOpOC.real,
         approval_program=approval, clear_program=clear,
         global_schema=transaction.StateSchema(4,1),
-        local_schema=transaction.StateSchema(0,0),
-        foreign_apps=[], foreign_assets=[]
+        local_schema=transaction.StateSchema(0,0)
     )
     signed = txn.sign(creator_private_key)
     txid = algod_client.send_transaction(signed)
-    print("Deploy TXID:", txid)
     result = transaction.wait_for_confirmation(algod_client, txid, 4)
-    app_id = result["application-index"]
-    print("Deployed App ID:", app_id)
+    print(result["application-index"])
 
 if __name__ == "__main__":
-    algod_address = "https://testnet-algorand.api.purestake.io/ps2"
-    algod_token = "<YOUR_TOKEN>"
-    client = algod.AlgodClient(algod_token, algod_address)
+    client = algod.AlgodClient("<YOUR_TOKEN>", "https://testnet-algorand.api.purestake.io/ps2")
     pk = sys.argv[1]
     teal_file = compile_contract()
-    deploy_app(client, pk, teal_file, teal_file)
+    deploy_app(client, pk, teal_file)
