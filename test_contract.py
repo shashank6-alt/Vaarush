f5f3d107226df303af27e2063b355e961540424b
D,Shashank <shashankdumpala6@gmail.com>
2025-10-15 23:34:36 +0530

    feat: initialize AlgoKit project structure for Vaarush

--- test_contract.py
+++
@@ -1,5 +0,0 @@
-from contracts.inheritance import inheritance_contract
-from pyteal import compileTeal, Mode
-
-compiled = compileTeal(inheritance_contract(), Mode.Application, version=5)
-print(compiled)
