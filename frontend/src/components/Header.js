54ed038b8ebc64ab53d5919ed18e17cb3aa2d317
D,Shashank <shashankdumpala6@gmail.com>
2025-10-19 23:55:30 +0530

    Init Header component and import React, router, auth hook

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,7 @@
+// Header.js (after commit 1)
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+export default function Header() {
+  return <header className="header"></header>;
+}
